48305363ecfc63a582fdfb909b9240678d0f487e
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-07-21 02:27:16 +0330

    feat(CSS): init Tailwind CSS

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1 class="text-3xl font-bold underline">Hello world! (with tailwind)</h1>
       <h1>hello world</h1>
     </div>
   );
