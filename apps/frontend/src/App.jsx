ff9d7f02d77537693e650a2556b34fd4e2dd7aaa
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-07-21 02:41:17 +0330

    feat(CSS): install daisy UI

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -3,6 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div role="alert" class="alert alert-info">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="h-6 w-6 shrink-0 stroke-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span>daisy UI works!</span>
+      </div>
       <h1 class="text-3xl font-bold underline">Hello world! (with tailwind)</h1>
       <h1>hello world</h1>
     </div>
