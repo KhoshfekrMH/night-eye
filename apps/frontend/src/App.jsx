cfd20080c02c90da763fb9b0e39536e0c6c31656
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-08-07 03:12:12 +0330

    improvement(UI): auto scroll top and some fixes

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./routes";
+import ScrollToTop from "./shared/util/ScrollToTop";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         {routes.map(({ path, element }, index) => (
           <Route key={index} path={path} element={element} />
--- apps/frontend/src/news/components/MetaInfo.jsx
+++ apps/frontend/src/news/components/MetaInfo.jsx
@@ -3,7 +3,7 @@ import { Calendar, EyeIcon } from "lucide-react";
 function MetaInfo({ badge, date, readCount }) {
   return (
     <div className="flex mb-6 text-sm text-accent gap-2">
-      <div className="badge badge-error">{badge}</div>
+      {badge && <div className="badge badge-error">{badge}</div>}
       <div className="flex items-center gap-1">
         <Calendar />
         <span>{date}</span>
