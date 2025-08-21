15f89f11d541ea35067d5a965afe69f15f60de47
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-08-22 01:37:44 +0330

    feat(api): temp sending email api
    warn: backend is not ready because of this reason I do not push backend
    codes and files

---
+++ apps/frontend/service/email.js
@@ -0,0 +1,20 @@
+export async function sendEmailApi(fromEmail, subject, message) {
+  try {
+    const res = await fetch("http://localhost:5000/api/send-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ fromEmail, subject, html: message }),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Email sending API failed:", error); //TODO: remove console.log
+    throw error;
+  }
+}
