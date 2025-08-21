8f2039e38ec877ceec9e7e6cf534a9f0e747ba5e
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-08-22 01:33:12 +0330

    refactor(util): add condition to LoadingSpinner

--- apps/frontend/src/shared/util/LoadingSpinner.jsx
+++ apps/frontend/src/shared/util/LoadingSpinner.jsx
@@ -1,7 +1,13 @@
-function LoadingSpinner() {
+function LoadingSpinner({ small = "false" }) {
+  if (small) {
+    return (
+      <span className="loading loading-spinner loading-sm text-secondary"></span>
+    );
+  }
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <span className="loading loading-spinner text-primary"></span>
+    <div classname="flex items-center justify-center h-screen">
+      <span classname="loading loading-spinner text-secondary"></span>
     </div>
   );
 }
