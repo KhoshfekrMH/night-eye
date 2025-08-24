a99a7bbf7d2ee83e08ca7f49a836c52f42912439
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-08-24 23:57:41 +0330

    improvement(Contact): add react helmet for page

--- apps/frontend/src/pages/Contact.jsx
+++ apps/frontend/src/pages/Contact.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PhoneCall, Mail } from "lucide-react";
+import { Helmet } from "react-helmet-async";
 import MainLayout from "../Layouts/MainLayout";
 import Alert from "../shared/components/UIElements/Alert";
 import { useAlert } from "../shared/context/AlertContext";
@@ -38,80 +39,85 @@ function Contact() {
   }
 
   return (
-    <MainLayout>
-      <div className="hero min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl text-secondary font-bold">Contact Us</h1>
-            <div className="py-6 max-w-lg">
-              If you have any questions or feedback, please don't hesitate to
-              contact us.
-              <div className="divider divider-primary"></div>
-              <ul>
-                <li className="py-2">
-                  <PhoneCall className="inline-block mr-2 text-primary" />
-                  <a href="tel:123-456-7890" className="hover:underline">
-                    123-456-7890
-                  </a>
-                </li>
-                <li>
-                  <Mail className="inline-block mr-2 text-primary" />
-                  <a
-                    href="mailto:admin@example.org"
-                    className="hover:underline"
-                  >
-                    admin@example.org
-                  </a>
-                </li>
-              </ul>
+    <>
+      <Helmet>
+        <title>Contact Us - Night Eye</title>
+      </Helmet>
+      <MainLayout>
+        <div className="hero min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl text-secondary font-bold">Contact Us</h1>
+              <div className="py-6 max-w-lg">
+                If you have any questions or feedback, please don't hesitate to
+                contact us.
+                <div className="divider divider-primary"></div>
+                <ul>
+                  <li className="py-2">
+                    <PhoneCall className="inline-block mr-2 text-primary" />
+                    <a href="tel:123-456-7890" className="hover:underline">
+                      123-456-7890
+                    </a>
+                  </li>
+                  <li>
+                    <Mail className="inline-block mr-2 text-primary" />
+                    <a
+                      href="mailto:admin@example.org"
+                      className="hover:underline"
+                    >
+                      admin@example.org
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <div className="card bg-base-200 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                {alert && <Alert type={alert.type} message={alert.message} />}
-                <form onSubmit={handleSubmit}>
-                  <label className="label py-2">Email</label>
-                  <input
-                    name="email"
-                    type="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="input validator"
-                    placeholder="mail@site.com"
-                  />
-                  <div className="validator-hint">
-                    Enter valid email address
-                  </div>
-                  <label className="label py-2">Subject</label>
-                  <input
-                    name="subject"
-                    type="text"
-                    value={formData.subject}
-                    onChange={handleChange}
-                    className="input validator"
-                  />
-                  <div className="validator-hint">Enter subject</div>
-                  <label className="label py-2">Message</label>
-                  <textarea
-                    name="message"
-                    type="text"
-                    value={formData.message}
-                    onChange={handleChange}
-                    className="textarea validator"
-                  />
-                  <div className="validator-hint">Enter message</div>
-                  <button className="btn btn-neutral mt-4" type="submit">
-                    {loading ? <LoadingSpinner small /> : "Send"}
-                  </button>
-                </form>
-              </fieldset>
+            <div className="card bg-base-200 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  {alert && <Alert type={alert.type} message={alert.message} />}
+                  <form onSubmit={handleSubmit}>
+                    <label className="label py-2">Email</label>
+                    <input
+                      name="email"
+                      type="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="input validator"
+                      placeholder="mail@site.com"
+                    />
+                    <div className="validator-hint">
+                      Enter valid email address
+                    </div>
+                    <label className="label py-2">Subject</label>
+                    <input
+                      name="subject"
+                      type="text"
+                      value={formData.subject}
+                      onChange={handleChange}
+                      className="input validator"
+                    />
+                    <div className="validator-hint">Enter subject</div>
+                    <label className="label py-2">Message</label>
+                    <textarea
+                      name="message"
+                      type="text"
+                      value={formData.message}
+                      onChange={handleChange}
+                      className="textarea validator"
+                    />
+                    <div className="validator-hint">Enter message</div>
+                    <button className="btn btn-neutral mt-4" type="submit">
+                      {loading ? <LoadingSpinner small /> : "Send"}
+                    </button>
+                  </form>
+                </fieldset>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </>
   );
 }
 
