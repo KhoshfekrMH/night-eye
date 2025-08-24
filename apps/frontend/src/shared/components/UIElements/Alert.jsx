8d9e3ca142fc309ec6d277b09f93c82bb6056419
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-08-24 23:47:57 +0330

    fix(Alert): fix color and type problem alert

--- apps/frontend/src/shared/components/UIElements/Alert.jsx
+++ apps/frontend/src/shared/components/UIElements/Alert.jsx
@@ -1,6 +1,13 @@
-function Alert({ type = success, message }) {
+function Alert({ type = "success", message }) {
+  const alertType = {
+    success: "alert-success",
+    error: "alert-error",
+    warning: "alert-warning",
+    info: "alert-info",
+  };
+
   return (
-    <div role="alert" className={`alert alert-${type} shadow-lg mb-4`}>
+    <div role="alert" className={`alert ${alertType[type]} shadow-lg mb-4`}>
       <span>{message}</span>
     </div>
   );
