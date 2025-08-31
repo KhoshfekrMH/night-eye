0f18a099b4e11bbe7501bb0fec9f1e423c9959bc
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-09-01 00:47:35 +0330

    feat(auth): basic form design with captcha

--- apps/frontend/src/dashboard/pages/Auth.jsx
+++ apps/frontend/src/dashboard/pages/Auth.jsx
@@ -1,5 +1,57 @@
+import { useState, useRef } from "react";
+import { Helmet } from "react-helmet-async";
+import MainLayout from "../../Layouts/MainLayout";
+import Alert from "../../shared/components/UIElements/Alert";
+import { useAlert } from "../../shared/context/AlertContext";
+import Captcha from "../../shared/util/Captcha";
+
 function Auth() {
-  return <h1>Auth</h1>;
+  const { showAlert, alert } = useAlert();
+  const captchaRef = useRef();
+  const [captchaValid, setCaptchaValid] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!captchaValid) {
+      return showAlert(
+        "Please complete the captcha before submitting",
+        "error",
+      );
+    }
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>Auth - Night Eye</title>
+      </Helmet>
+      <MainLayout>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <fieldset className="fieldset bg-base-200 border-base-100 rounded-box w-xs border p-4">
+            {alert && <Alert {...alert} />}
+            <form onSubmit={handleSubmit}>
+              <legend className="fieldset-legend">Login</legend>
+
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <div className="mt-4">
+                <Captcha setCaptchaValid={setCaptchaValid} ref={captchaRef} />
+              </div>
+
+              <button className="btn btn-neutral mt-4" type="submit">
+                Login
+              </button>
+            </form>
+          </fieldset>
+        </div>
+      </MainLayout>
+    </>
+  );
 }
 
 export default Auth;
