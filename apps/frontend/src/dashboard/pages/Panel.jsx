298b3c395a8ab0a6da119dda618897be8cf70af9
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-09-04 02:32:14 +0330

    feat(panel-auth): add panel structure with basic component for news tab

--- apps/frontend/src/dashboard/pages/Panel.jsx
+++ apps/frontend/src/dashboard/pages/Panel.jsx
@@ -2,6 +2,12 @@ import { Helmet } from "react-helmet-async";
 import PanelLayout from "../../Layouts/PanelLayout";
 
 function Panel() {
+  return <h1>Panel</h1>;
+import { Helmet } from "react-helmet-async";
+import PanelLayout from "../../Layouts/PanelLayout";
+import PanelNewsTable from "../components/NewsTab/PanelNewsTable";
+
+function Panel() {
   return (
     <>
       <Helmet>
@@ -16,9 +22,30 @@ function Panel() {
             aria-label="📰 News"
             defaultChecked
           />
-          <div className="tab-content bg-base-100 border-base-300 p-6">
-            Tab content 1
-          </div>
+          <section className="tab-content bg-base-100 border-base-300 p-6">
+            <div className="tabs tabs-border">
+              <input
+                type="radio"
+                name="news_tab"
+                className="tab"
+                aria-label="🗂️ News Table"
+                defaultChecked
+              />
+              <article className="tab-content border-base-300 bg-base-100 p-10">
+                <PanelNewsTable />
+              </article>
+
+              <input
+                type="radio"
+                name="news_tab"
+                className="tab"
+                aria-label="✏️ Create News"
+              />
+              <article className="tab-content border-base-300 bg-base-100 p-10">
+                Create News
+              </article>
+            </div>
+          </section>
 
           <input
             type="radio"
@@ -26,9 +53,9 @@ function Panel() {
             className="tab text-secondary hover:text-primary transition font-bold"
             aria-label="👤 Members"
           />
-          <div className="tab-content bg-base-100 border-base-300 p-6">
-            Tab content 2
-          </div>
+          <section className="tab-content bg-base-100 border-base-300 p-6">
+            {/* TODO: WIP tab feature! */}
+          </section>
 
           <input
             type="radio"
@@ -36,9 +63,9 @@ function Panel() {
             className="tab text-secondary hover:text-primary transition font-bold"
             aria-label="⚙️ Settings"
           />
-          <div className="tab-content bg-base-100 border-base-300 p-6">
-            Tab content 3
-          </div>
+          <section className="tab-content bg-base-100 border-base-300 p-6">
+            {/* TODO: WIP tab feature! */}
+          </section>
         </div>
       </PanelLayout>
     </>
--- apps/frontend/src/pages/Home.jsx
+++ apps/frontend/src/pages/Home.jsx
@@ -5,86 +5,92 @@ import Hero from "../shared/components/UIElements/Hero/Hero";
 import ScrollTabs from "../shared/components/UIElements/Tab/ScrollTabs";
 import NewsList from "../news/pages/NewsList";
 import { news } from "../shared/dummy"; //TODO: dummy data
+import { Helmet } from "react-helmet-async";
 
 function Home() {
   const categories = ["nasa", "space", "tech"];
   const heroNews = news.find((n) => n.hero === true);
 
   return (
-    <MainLayout>
-      <main className="flex flex-col items-center justify-center px-5 mx-auto my-8 w-full max-w-6xl gap-16">
-        <section arial-label="Hero featured news">
-          <Hero
-            {...heroNews}
-            description={heroNews.description.slice(0, 100) + "..."}
-            image={heroNews.mainImage}
-            buttonLink={`/news/${heroNews.slug}`}
-            buttonText="Read More"
-          />
-        </section>
+    <>
+      <Helmet>
+        <title>Night Eye - Sci-Fi News</title>
+      </Helmet>
+      <MainLayout>
+        <main className="flex flex-col items-center justify-center px-5 mx-auto my-8 w-full max-w-6xl gap-16">
+          <section arial-label="Hero featured news">
+            <Hero
+              {...heroNews}
+              description={heroNews.description.slice(0, 100) + "..."}
+              image={heroNews.mainImage}
+              buttonLink={`/news/${heroNews.slug}`}
+              buttonText="Read More"
+            />
+          </section>
 
-        <section arial-label="News categories navigation">
-          <ScrollTabs categories={categories} />
-        </section>
+          <section arial-label="News categories navigation">
+            <ScrollTabs categories={categories} />
+          </section>
 
-        {categories.map((categories) => {
-          const badgeNews = news
-            .filter((n) => n.category === categories && n.badge)
-            .sort((a, b) => new Date(b.date) - new Date(a.date))
-            .slice(0, 1);
+          {categories.map((categories) => {
+            const badgeNews = news
+              .filter((n) => n.category === categories && n.badge)
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .slice(0, 1);
 
-          const normalNews = news
-            .filter((n) => n.category === categories && !n.badge)
-            .sort((a, b) => new Date(b.date) - new Date(a.date))
-            .slice(0, 3);
+            const normalNews = news
+              .filter((n) => n.category === categories && !n.badge)
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .slice(0, 3);
 
-          return (
-            <section
-              id={categories}
-              key={categories}
-              aria-label={`${categories} news`}
-              className="max-w-4xl"
-            >
-              <header className="flex flex-row items-center justify-between">
-                <h2 className="text-4xl text-secondary font-bold Capitalize hover:underline">
-                  <Link to={`/archive/${categories}`}>
-                    {categories === "nasa"
-                      ? "🚀 Nasa"
-                      : categories === "space"
-                        ? "🪐 Space"
-                        : "🦾 Tech"}
-                  </Link>
-                </h2>
-              </header>
-              <div className="divider divider-primary"></div>
-              {badgeNews.length > 0 && (
-                <article arial-label={`${categories} badge news`}>
-                  <NewsList
-                    category={categories}
-                    size="large"
-                    newsType="badged"
-                  />
-                </article>
-              )}
+            return (
+              <section
+                id={categories}
+                key={categories}
+                aria-label={`${categories} news`}
+                className="max-w-4xl"
+              >
+                <header className="flex flex-row items-center justify-between">
+                  <h2 className="text-4xl text-secondary font-bold Capitalize hover:underline">
+                    <Link to={`/archive/${categories}`}>
+                      {categories === "nasa"
+                        ? "🚀 Nasa"
+                        : categories === "space"
+                          ? "🪐 Space"
+                          : "🦾 Tech"}
+                    </Link>
+                  </h2>
+                </header>
+                <div className="divider divider-primary"></div>
+                {badgeNews.length > 0 && (
+                  <article arial-label={`${categories} badge news`}>
+                    <NewsList
+                      category={categories}
+                      size="large"
+                      newsType="badged"
+                    />
+                  </article>
+                )}
 
-              {normalNews.length > 0 && (
-                <aside
-                  arial-label={`${categories} normal news`}
-                  className="mt-8"
-                >
-                  <NewsList
-                    category={categories}
-                    size="wrap"
-                    pagination={false}
-                    newsType="normal"
-                  />
-                </aside>
-              )}
-            </section>
-          );
-        })}
-      </main>
-    </MainLayout>
+                {normalNews.length > 0 && (
+                  <aside
+                    arial-label={`${categories} normal news`}
+                    className="mt-8"
+                  >
+                    <NewsList
+                      category={categories}
+                      size="wrap"
+                      pagination={false}
+                      newsType="normal"
+                    />
+                  </aside>
+                )}
+              </section>
+            );
+          })}
+        </main>
+      </MainLayout>
+    </>
   );
 }
 
