bca4f973063232385f96dc042cf5db268c098ff9
Pouya Khoshfekr <Khoshfekr2001@protonmail.com>
2025-09-11 02:46:53 +0330

    refactor(panel-auth): some small improvment on filing and naming for news table

--- apps/frontend/src/dashboard/components/NewsTab/PanelNewsTable.jsx
+++ apps/frontend/src/dashboard/components/NewsTab/PanelNewsTable.jsx
@@ -1,12 +1,18 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../../../shared/context/AuthContext";
-import { news } from "../../../shared/dummy";
-import NewsTableRow from "./rows/NewsTableRow";
+import { news } from "../../../shared/dummy"; //TODO: connect backend for panel news filtered
+import NewsTableBody from "./table/layouts/NewsTableBody";
+import NewsTableHeader from "./table/layouts/NewsTableHeader";
+import NewsTableActions from "./table/util/NewsTableActions";
 
 export default function PanelNewsTable() {
   const { id: userId, isAdmin, isOwner, isWriter } = useContext(AuthContext);
 
-  const panelNewsFiltered = news.filter((n) => {
+  const [selectedIds, setSelectedIds] = useState([]);
+  const [dateSort, setDateSort] = useState("desc");
+  const [readCountSort, setReadCountSort] = useState(null);
+
+  let panelNewsFiltered = news.filter((n) => {
     if (isOwner || isAdmin) {
       return true;
     } else if (isWriter) {
@@ -16,7 +22,21 @@ export default function PanelNewsTable() {
     }
   });
 
-  const [selectedIds, setSelectedIds] = useState([]);
+  if (dateSort) {
+    panelNewsFiltered = [...panelNewsFiltered].sort((a, b) =>
+      dateSort === "asc"
+        ? new Date(a.date) - new Date(b.date)
+        : new Date(b.date) - new Date(a.date),
+    );
+  }
+
+  if (readCountSort) {
+    panelNewsFiltered = [...panelNewsFiltered].sort((a, b) =>
+      readCountSort === "asc"
+        ? a.readCount - b.readCount
+        : b.readCount - a.readCount,
+    );
+  }
 
   function toggleAll(event) {
     if (event.target.checked) {
@@ -34,44 +54,50 @@ export default function PanelNewsTable() {
 
   return (
     <div className="overflow-x-auto">
+      <div className="flex flex-row mb-5">
+        <label className="label">
+          <span className="label-text font-bold">Bulk Actions: </span>
+        </label>{" "}
+        {/*TODO: add bulk actions*/}
+        <NewsTableActions
+          onDelete={() => console.log("Bulk Delete", selectedIds)}
+          onArchive={() => console.log("Bulk Archive", selectedIds)}
+          actions={["delete", "archive"]}
+          mode="global"
+        />
+        <div className="justify-end ml-auto">
+          <label className="label">
+            <span className="label-text font-bold">
+              {selectedIds.length > 0 && "Selected: " + selectedIds.length}
+            </span>
+          </label>
+        </div>
+      </div>
+
       <table className="table w-full">
-        <thead>
-          <tr>
-            <th>
-              <label>
-                <input
-                  type="checkbox"
-                  className="checkbox"
-                  onChange={toggleAll}
-                  checked={
-                    selectedIds.length > 0 &&
-                    selectedIds.length === panelNewsFiltered.length
-                  }
-                />
-              </label>
-            </th>
-            <th>Date</th>
-            <th>Category</th>
-            <th>News</th>
-            <th>Status</th>
-            <th>Tags</th>
-            <th>Read Count</th>
-            <th>Description</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {panelNewsFiltered.map((n) => {
-            return (
-              <NewsTableRow
-                key={n.id}
-                newsItem={n}
-                isSelected={selectedIds.includes(n.id)}
-                toggleRow={() => toggleRow(n.id)}
-              />
-            );
-          })}
-        </tbody>
+        <NewsTableHeader
+          panelNews={panelNewsFiltered}
+          toggleAll={toggleAll}
+          checked={
+            selectedIds.length > 0 &&
+            selectedIds.length === panelNewsFiltered.length
+          }
+          dateSort={dateSort}
+          setDateSort={setDateSort}
+          readCountSort={readCountSort}
+          setReadCountSort={setReadCountSort}
+        />
+
+        {panelNewsFiltered.map((n) => {
+          return (
+            <NewsTableBody
+              key={n.id}
+              newsItem={n}
+              isSelected={selectedIds.includes(n.id)}
+              toggleRow={() => toggleRow(n.id)}
+            />
+          );
+        })}
       </table>
     </div>
   );
